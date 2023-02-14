91c83754f1f2987c87a65c99ecb9c54f8a4992eb
Emanoelgeman <emanoel.germano.malheiros@gmail.com>
2023-02-13 21:13:01 -0300

    Exercise 004 Salário bruto IF

--- Exercise004.py
+++ Exercise004.py
@@ -1,6 +1,6 @@
-#Exercise 004: Salário bruto (IF)
-#Dev: Emanoel G Malheiros
-#Data inicio: 08/02/2023
+# Exercise 004: Salário bruto (IF)
+# Dev: Emanoel G Malheiros
+# Data inicio: 08/02/2023
 
 while True:
     try:
@@ -11,21 +11,17 @@ while True:
 imposto = float(0.93)
 
 if salariobruto <= 350:
-    salariototal= salariobruto * imposto + 100
-    print("O seu salário total é de:",salariototal)
+    salariototal = salariobruto * imposto + 100
+    print("O seu salário total é de:", salariototal)
 
-elif salariobruto >=351 and salariobruto <=600:
-    salariototal= salariobruto * imposto + 75
-    print("O seu salário total é de:",salariototal)
+elif salariobruto >= 351 and salariobruto <= 600:
+    salariototal = salariobruto * imposto + 75
+    print("O seu salário total é de:", salariototal)
 
-elif salariobruto >=601 and salariobruto <=900:
-    salariototal= salariobruto * imposto + 50
-    print("O seu salário total é de:",salariototal)
+elif salariobruto >= 601 and salariobruto <= 900:
+    salariototal = salariobruto * imposto + 50
+    print("O seu salário total é de:", salariototal)
 
 else:
-    salariototal= salariobruto * imposto + 35
-    print("O seu salário total é de:",salariototal)
-
-
-
-
+    salariototal = salariobruto * imposto + 35
+    print("O seu salário total é de:", salariototal)
